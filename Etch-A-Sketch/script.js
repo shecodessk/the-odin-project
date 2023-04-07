94008450467e0e51b8023adef216a69ae04a1fb2
Stephany <119885319+shecodessk@users.noreply.github.com>
2023-04-07 12:32:12 -0700

    Create a 16x16 grid with JS

--- Etch-A-Sketch/script.js
+++ Etch-A-Sketch/script.js
@@ -1,4 +1,12 @@
-const content = document.querySelector('#content');
-const grid = document.createElement('div')
+const content = document.getElementById('content');
 
-content.appendChild(grid)
+function makeGrid(rows, cols){
+  content.style.setProperty('--grid-rows', rows)
+  content.style.setProperty('--grid-cols', cols);
+  for(c = 0; c < rows*cols; c++){
+    let cell = document.createElement("div");
+    content.appendChild(cell).className = "grid-item";
+  }
+}
+makeGrid(16, 16);
+
